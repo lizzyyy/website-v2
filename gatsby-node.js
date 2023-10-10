47aa13c74a590540fa0a6c0ddb73202e209fc11f
Lizzy Wang <yuchen.w@berkeley.edu>
2023-10-10 00:29:12 -0700

    fix canvas

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,12 +1,17 @@
-const webpack = require('webpack');
+// const webpack = require('webpack');
 
-exports.onCreateWebpackConfig = ({ actions }) => {
-    actions.setWebpackConfig({
-      plugins: [
-        new webpack.IgnorePlugin({
-          resourceRegExp: /canvas|jsdom/,
-          contextRegExp: /konva/,
-        }),
-      ],
-    });
-  };
+// exports.onCreateWebpackConfig = ({ stage,
+//   rules,
+//   loaders,
+//   plugins,
+//   actions, }) => {
+//     actions.setWebpackConfig({
+//       plugins: [
+//         new webpack.IgnorePlugin({
+//           resourceRegExp: /canvas|jsdom/,
+//           contextRegExp: /konva/,
+//         }),
+//       ],
+//       externals: ['canvas']
+//     });
+//   };
